891e11d45b4ccd472e6a4bcb2c1af8f52eef6186
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-12-06 00:40:21 -0300

    feat: decode token function

--- src/utils/token.js
+++ src/utils/token.js
@@ -1,15 +1,22 @@
 import jwt from 'jsonwebtoken';
 import 'dotenv/config';
 
+const secret = process.env.JWT_SECRET;
+
 const generateToken = (payload) => {
   const jwtConfig = {
     expiresIn: '1d',
     algorithm: 'HS256',
   };
-  const secret = process.env.JWT_SECRET;
 
   const token = jwt.sign(payload, secret, jwtConfig);
   return token;
+};
+
+export const decodeToken = (token) => {
+  const decoded = jwt.verify(token, secret);
+
+  return decoded.data;
 };
 
 export default generateToken;
